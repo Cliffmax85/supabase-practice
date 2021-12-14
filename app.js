d6343ac03abde2fc24c805266ba52b3590d1ed7f
cliffmax85 <cliffmax85@gmail.com>
2021-12-14 15:28:36 -0800

    first commit

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 // import functions and grab DOM elements
-
+import { getCars, getMovies } from './fetch-utils.js';
+import { renderCar, renderMovie } from './render-utils.js';
 // let state
-
+const carsListContainer = document.getElementById('cars-list-container');
+const moviesListContainer = document.getElementById('movies-list-container');
 // set event listeners 
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+window.addEventListener('load', async() => {
+    const cars = await getCars();
+
+    for (let car of cars) {
+        const carsEl = renderCar(car);
+        carsListContainer.append(carsEl);
+    }
+});
+
+window.addEventListener('load', async() => {
+    const movies = await getMovies();
+
+    for (let movie of movies) {
+        const moviesEl = renderMovie(movie);
+        moviesListContainer.append(moviesEl);
+    }
+});
---
+++ render-utils.js
@@ -0,0 +1,36 @@
+export function renderCar(car) {
+    const div = document.createElement('div');
+    const makeEl = document.createElement('p');
+    const colorEl = document.createElement('p');
+    const modelEl = document.createElement('p');
+
+    div.classList.add('car-item');
+
+    if (car.color === car.color) {
+        div.style.backgroundColor = car.color;
+    } else (car.color !== car.color); 
+    modelEl.style.backgroundColor = 'purple';
+
+    if (car.model[0] === 'C') {
+        div.style.transform = 'rotate(45deg)';
+    }
+
+    makeEl.textContent = car.make;
+    colorEl.textContent = car.color;
+    modelEl.textContent = car.model;
+    div.append(makeEl, colorEl, modelEl);
+
+    return div;
+
+}
+
+export function renderMovie(movie) {
+    const div = document.createElement('div');
+    const titleEl = document.createElement('p');
+    const lengthEl = document.createElement('p');
+    const genreEl = document.createElement('p');
+    const bestPictureEl = document.createElement('p');
+
+    div.classList.add('movie-item');
+    
+}
